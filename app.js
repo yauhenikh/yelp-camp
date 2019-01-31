b8e0423685a1c3b0b702d0c9bf6686389dc1380d
Yauheni Khotsin <khotin.13.nomer@gmail.com>
2019-01-31 16:47:27 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb://yauhenikh:Yallwant4@ds257551.mlab.com:57551/yelp-camp-4", {useNewUrlParser: true});
+// mongodb://yauhenikh:Yallwant4@ds257551.mlab.com:57551/yelp-camp-4
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
